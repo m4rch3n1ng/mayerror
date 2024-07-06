d8b1afbaa4a5aa7b8a5d7e42399b83ee4fb89204
may <m4rch3n1ng@gmail.com>
2024-07-06 19:21:04 +0200

    very early derive macro

---
+++ examples/test.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+
+use mayerror::MayError;
+
+#[derive(MayError)]
+struct MayError {
+	#[location]
+	location: &'static std::panic::Location<'static>,
+	#[code]
+	thing: MayErrorCode,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum MayErrorCode {
+	#[error("source error")]
+	Source(#[from] MayValError),
+	#[error("unit error")]
+	Unit,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("may val error")]
+struct MayValError;
+
+fn main() -> Result<(), MayError> {
+	let () = Err(MayErrorCode::Source(MayValError))?;
+	Ok(())
+}
